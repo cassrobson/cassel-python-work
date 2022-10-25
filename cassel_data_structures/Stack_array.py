1a99190889f86f007c287f558b2f739117f15d26
Cassel Robson <116671665+cassrobson@users.noreply.github.com>
2022-10-25 17:49:56 -0400

    Create Stack_array.py

---
+++ cassel_data_structures/Stack_array.py
@@ -0,0 +1,149 @@
+"""
+-------------------------------------------------------
+Stack_Array
+-------------------------------------------------------
+Author:  Cassel Robson
+ID:      210507000
+Email:   robs7000@mylaurier.ca
+__updated__ = "2022-01-28"
+-------------------------------------------------------
+"""
+from copy import deepcopy
+
+
+class Stack:
+
+    def __init__(self):
+        """
+        -------------------------------------------------------
+        Initializes an is_empty stack. Data is stored in a Python list.
+        Use: s = Stack()
+        -------------------------------------------------------
+        Returns:
+            a new Stack object (Stack)
+        -------------------------------------------------------
+        """
+        self._values = []
+
+    def is_empty(self):
+        """
+        -------------------------------------------------------
+        Determines if the stack is empty.
+        Use: b = s.is_empty()
+        -------------------------------------------------------
+        Returns:
+            True if the stack is empty, False otherwise
+        -------------------------------------------------------
+        """
+
+        return len(self._values) == 0
+
+    def push(self, value):
+        """
+        -------------------------------------------------------
+        Pushes a copy of value onto the top of the stack.
+        Use: s.push(value)
+        -------------------------------------------------------
+        Parameters:
+            value - a data element (?)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+
+        copy_value = deepcopy(value)
+        self._values.append(copy_value)
+
+    def pop(self):
+        """
+        -------------------------------------------------------
+        Pops and returns the top of stack. The value is removed
+        from the stack. Attempting to pop from an empty stack
+        throws an exception.
+        Use: value = s.pop()
+        -------------------------------------------------------
+        Returns:
+            value - the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot pop from an empty stack"
+
+        value = self._values.pop()
+
+        return value
+
+    def peek(self):
+        """
+        -------------------------------------------------------
+        Returns a copy of the value at the top of the stack.
+        Attempting to peek at an empty stack throws an exception.
+        Use: value = s.peek()
+        -------------------------------------------------------
+        Returns:
+            value - a copy of the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot peek at an empty stack"
+
+        lst = deepcopy(self._values)
+        value = lst[-1]
+        return value
+
+    def __iter__(self):
+        """
+        FOR TESTING ONLY
+        -------------------------------------------------------
+        Generates a Python iterator. Iterates through the stack
+        from top to bottom.
+        Use: for v in s:
+        -------------------------------------------------------
+        Returns:
+            value - the next value in the stack (?)
+        -------------------------------------------------------
+        """
+        for value in self._values[::-1]:
+            yield value
+
+    def combine(self, source1, source2):
+        """
+        -------------------------------------------------------
+        Combines two source stacks into the current target stack.
+        When finished, the contents of source1 and source2 are interlaced
+        into target and source1 and source2 are empty.
+        Use: target.combine(source1, source2)
+        -------------------------------------------------------
+        Parameters:
+            source1 - an array-based stack (Stack)
+            source2 - an array-based stack (Stack)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+
+        while len(source1._values) and len(source2._values) > 0:
+            self._values.append(deepcopy(source1._values.pop()))
+            self._values.append(deepcopy(source2._values.pop()))
+
+        if len(source1._values) > 0:
+            while len(source1._values) > 0:
+                self._values.append(deepcopy(source1._values.pop()))
+
+        if len(source2._values) > 0:
+            while len(source2._values) > 0:
+                self._values.append(deepcopy(source2._values.pop()))
+
+    def reverse(self):
+        """
+        -------------------------------------------------------
+        Reverses the contents of the source stack.
+        Use: source.reverse()
+        -------------------------------------------------------
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        items = []
+        while not len(self._values) == 0:
+            items.append(self._values.pop())
+        for item in items:
+            self._values.append(deepcopy(item))
